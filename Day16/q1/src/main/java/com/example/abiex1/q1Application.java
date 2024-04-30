99764d9568f639c72a6c19dbb4d0fdbe1ae12cad
Rengesh <123830137+Srirengeshwar@users.noreply.github.com>
2024-04-30 09:30:48 +0530

    Add files via upload

---
+++ Day16/q1/src/main/java/com/example/abiex1/q1Application.java
@@ -0,0 +1,14 @@
+package com.example.abiex1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class q1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(q1Application.class, args);
+		
+	}
+
+}
---
+++ Day16/q2/src/main/java/com/example/abiex2/q2Application.java
@@ -0,0 +1,14 @@
+package com.example.abiex1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class q2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(q2Application.class, args);
+		
+	}
+
+}
